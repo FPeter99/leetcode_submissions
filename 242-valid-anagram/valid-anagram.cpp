0fc4f9ed1876babc6ca9097097bc541f3512a3e0
Forgács Péter <forgacs.peter08@gmail.com>
2026-09-19 10:29:42 +0200

    Time: 4 ms (33.72%) | Memory: 9.92 MB (11.22%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if (s.size() != t.size()) {
+            return false;
+        }
+        
+        std::unordered_map<char, int> count;
+        
+
+        for (char c : s) {
+            count[c]++;
+        }
+        
+
+        for (char c : t) {
+            if (count.find(c) == count.end() || count[c]-- <= 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
