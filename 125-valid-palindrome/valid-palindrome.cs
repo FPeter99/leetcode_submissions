786193455cfdedd672792d5d068c15702d13a9e4
Peter Forgacs <forgacs.peter08@gmail.com>
2026-09-22 20:51:53 +0200

    Time: 20 ms (45.40%) | Memory: 45.2 MB (48.18%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int i = 0;
+        int j = s.Length-1;
+
+        while(j>i)
+        {
+            while(j>i && !char.IsAsciiLetterOrDigit((s[i]))){i++;}
+            while(j>i && !char.IsAsciiLetterOrDigit((s[j]))){j--;}
+            if(char.ToLower(s[i]) != char.ToLower(s[j])){return false;}
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
